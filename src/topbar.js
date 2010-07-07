e4fb8fd0f7a35b19dcfccbba0bf6e01501013489
Sargento <marcio.santana@sargento.glb.com>
2010-07-07 10:30:41 -0300

    Incluindo várias funcionalidades!!!

--- src/topbar.js
+++ src/topbar.js
@@ -1,19 +1,58 @@
 $.widget('ui.topbar', {
     options: {
-        'leftClass': 'left',
-        'rightClass': 'right',
-        'linkClass': 'link'
+        'leftClass': '',
+        'rightClass': '',
+        'linkClass': '',
+        'useHtml': false,
+        'leftLinks': [],
+        'rightLinks': [],
+        'leftSeparator': '',
+        'rightSeparator': ''
     },
     _create: function() {
-        this.leftDiv = this._getDiv('leftClass').appendTo(this.element);
-        this.rightDiv = this._getDiv('rightClass').appendTo(this.element);
+        if (this.options.useHtml) {
+            this._validateDivPresence('left');
+            this._validateDivPresence('right');
+        }
+        else {
+            this.element.html('');
+            this._createDiv('left');
+            this._createDiv('right');
+        }
+        this.element.prependTo($('body'));
     },
     destroy: function() {
-    	this.leftDiv.remove();
-    	this.rightDiv.remove();
+        if (!this.options.useHtml) {
+        	this.leftDiv.remove();
+        	this.rightDiv.remove();
+	    }
     	$.Widget.prototype.destroy.apply(this, arguments);
 	},
-    _getDiv: function(cssClass) {
-        return $('<div class="{class}"></div>'.replace('{class}', this.options[cssClass]))
+    _validateDivPresence: function(position) {
+        if (this.element.find('div.' + position).length == 0) {
+            throw('The element must have a div with a class "{position}".'.replace('{position}', position))
+        }        
+    },
+    _createDiv: function(position) {
+        this[position + 'Div'] = this._getDiv(position).appendTo(this.element);
+        this._putLinksInDiv(position);
+    },
+    _getDiv: function(position) {
+        return $('<div class="{position} {class}"></div>'
+                    .replace('{class}', this.options[position + 'Class'])
+                    .replace('{position}', position)
+                );
+    },
+    _putLinksInDiv: function(position) {
+        for (var i = 0; i < this.options[position + 'Links'].length; i++) {
+            item = this.options[position + 'Links'][i];
+            var $el = $('<a></a>');
+            $el.attr('href', item.href);
+            if (item.current) {
+                $el = $('<b></b>')
+            }
+            $el.text(item.text);
+            $el.appendTo(this[position + 'Div']);
+        }
     }
 });
--- test/topbar_test.js
+++ test/topbar_test.js
@@ -13,16 +13,19 @@ test('Barra é destruida com sucesso',
 function() {
     $('#topbar').topbar();
     $('#topbar').topbar('destroy');
-    equals($('#topbar').html(), '');
+    equals($('#topbar').html(), '', "O html do elemento deve estar vazio ao chamar destroy");
 });
 
 test('Barra criada possui opcoes padrao',
 function() {
     $('#topbar').topbar();
     
-    equals($('#topbar').topbar('option', 'leftClass'), 'left');
-    equals($('#topbar').topbar('option', 'rightClass'), 'right');
-    equals($('#topbar').topbar('option', 'linkClass'), 'link');
+    equals($('#topbar').topbar('option', 'leftClass'), '', 'leftClass');
+    equals($('#topbar').topbar('option', 'rightClass'), '', 'rightClass');
+    equals($('#topbar').topbar('option', 'linkClass'), '', 'linkClass');
+    equals($('#topbar').topbar('option', 'useHtml'), false, 'useHtml');
+    equals($('#topbar').topbar('option', 'leftSeparator'), '', 'useSeparatorInLeft');
+    equals($('#topbar').topbar('option', 'rightSeparator'), '', 'useSeparatorInRight');
     
     $('#topbar').topbar('destroy');
 });
@@ -31,7 +34,7 @@ test('Barra sobrescreve opcao padrao',
 function() {
     $('#topbar').topbar({'leftClass': 'other-left'});
     
-    equals($('#topbar').topbar('option', 'leftClass'), 'other-left');
+    equals($('#topbar').topbar('option', 'leftClass'), 'other-left', 'leftClass deve ser alterada para "other-left"');
 
     $('#topbar').topbar('destroy');
 });
@@ -40,7 +43,7 @@ test('Barra acrescenta opcao',
 function() {
     $('#topbar').topbar({'class': 'new-class'});
     
-    equals($('#topbar').topbar('option', 'class'), 'new-class');
+    equals($('#topbar').topbar('option', 'class'), 'new-class', 'Deve existir nova class "new-class"');
 
     $('#topbar').topbar('destroy');
 });
@@ -63,4 +66,128 @@ function() {
     equals($div.find('div.new-right').length, 1, 'Deve conter um div.new-right');
 
     $('#topbar').topbar('destroy');
+});
+
+test('Barra apaga html original antes de criar markup',
+function() {
+    $('#topbar').append($('<div id="naoDeveExistir"></div>'))
+    $('#topbar').topbar();
+
+    var $div = $('#topbar')
+    equals($div.find('div#naoDeveExistir').length, 0, 'Não deve conter um div#naoDeveExistir.');
+
+    $('#topbar').topbar('destroy');
+});
+
+test('Barra usa html original caso a opcao useHtml seja true.',
+function() {
+    var $div = $('#topbar')
+    $div.append('<div class="left other-left"></div><div class="right other-right"></div>');
+    $('#topbar').topbar({useHtml: true});
+
+    equals($div.find('div.other-left').length, 1, 'Deve conter um div.other-left');
+    equals($div.find('div.other-right').length, 1, 'Deve conter um div.other-right');
+
+    $('#topbar').topbar('destroy');
+    $div.html('');
+});
+
+test('Valida a existencia de um div.left caso useHtml seja true.',
+function() {
+    expect(1);
+    var $div = $('#topbar')
+    $div.append('<div class="other-left"></div><div class="right other-right"></div>');
+    try {
+        $('#topbar').topbar({useHtml: true});
+    }
+    catch(ex) {
+        equals(ex, 'The element must have a div with a class "left".', 'Deve disparar um erro com a mensagem correta')
+    }
+    
+    $('#topbar').topbar('destroy');
+    $div.html('');
+});
+
+test('Valida a existencia de um div.right caso useHtml seja true.',
+function() {
+    expect(1);
+    var $div = $('#topbar')
+    $div.append('<div class="left other-left"></div><div class="other-right"></div>');
+    try {
+        $('#topbar').topbar({useHtml: true});
+    }
+    catch(ex) {
+        equals(ex, 'The element must have a div with a class "right".', 'Deve disparar um erro com a mensagem correta')
+    }
+    
+    $('#topbar').topbar('destroy');
+    $div.html('');
+});
+
+test('Widget insere html no topo da página',
+function () {
+    $('#topbar').topbar();
+    
+    var $div = $('body div:first');
+    equals($('#topbar').html(), $div.html());    
+    $('#topbar').topbar('destroy');
+});
+
+test('Recebe json com links e exibe na esquerda',
+function () {
+    $('#topbar').topbar({
+        'leftLinks': [
+            {'text': 'Um link', 'href': '/umlink/'},
+            {'text': 'Dois link', 'href': '/doislink/'},
+            {'text': 'Três link', 'href': '/treslink/'},
+        ]
+    });
+    
+    var $div = $('#topbar')
+    equals($div.find('div.left a').length, 3, 'Devem existir 3 links no div da esquerda');
+    var href = $div.find('div.left a')[2].href.replace(window.location.protocol + "//" + window.location.host, '');
+    equals(href, '/treslink/', 'O terceiro link deve apontar para /treslink/');
+    $('#topbar').topbar('destroy');
+});
+
+test('Recebe json com links e exibe na direita',
+function () {
+    $('#topbar').topbar({
+        'rightLinks': [
+            {'text': 'Quatro link', 'href': '/quatrolink/'},
+            {'text': 'Cinco link', 'href': '/cincolink/'},
+            {'text': 'Seis link', 'href': '/seislink/'},
+            {'text': 'Sete link', 'href': '/setelink/'},
+        ]
+    });
+    
+    var $div = $('#topbar')
+    equals($div.find('div.right a').length, 4, 'Devem existir 4 links no div da direita');
+    var href = $div.find('div.right a')[2].href.replace(window.location.protocol + "//" + window.location.host, '');
+    equals(href, '/seislink/', 'O terceiro link deve apontar para /seislink/');
+
+    $('#topbar').topbar('destroy');
+});
+
+test('Barra nao monta link para item marcado como current',
+function () {
+    $('#topbar').topbar({
+        'leftLinks': [
+            {'text': 'Um link', 'href': '/umlink/'},
+            {'text': 'Dois link', 'href': '/doislink/'},
+            {'text': 'Três link', 'href': '/treslink/'},
+        ],
+        'rightLinks': [
+            {'text': 'Quatro link', 'href': '/quatrolink/'},
+            {'text': 'Cinco link', 'current': true},
+            {'text': 'Seis link', 'href': '/seislink/'},
+            {'text': 'Sete link', 'href': '/setelink/'},
+        ]
+    });
+
+    var $div = $('#topbar')
+    equals($div.find('div.right a').length, 3, 'Devem existir 3 links no div da direita');
+    var b = $div.find('div.right b');
+    equals($div.find('div.right b').text(), 'Cinco link', 'O quinto elemento deve ser um b com texto "Cinco link"');
+
 });
